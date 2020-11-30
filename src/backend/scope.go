0b3504f355c3bdfaa1d7c270bf10143c33c57ce2
Biel <bielp07@gmail.com>
2020-11-30 11:29:55 -0300

    add 'Scope' struct

--- src/backend/scope.go
+++ src/backend/scope.go
@@ -1,8 +1,25 @@
 package scope
 
-import "context"
+import (
+	"context"
+	"sync"
+	project "./project"
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+const mName = "scope"
 
 type Repository interface {
   UpSettings(ctx context.Context, module string, settings interface{}) error
   FindSettingsByModule(ctx context.Context, module string, settings interface{}) error
 }
+
+type Scope struct {
+  rules	[]Rule
+  repo	Repository
+  mutex	sync.RWMutex
+}
+
+//@@@TODO: 'Rule' struct.
